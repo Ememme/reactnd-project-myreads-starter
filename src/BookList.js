c7737c73d90b545aff54b8954266d7afaccb3796
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-07-24 18:54:56 +0200

    Refactor to Stateless Functional Component

--- src/BookList.js
+++ src/BookList.js
@@ -1,37 +1,41 @@
 import React, {Component} from 'react'
 import BookShelf from './BookShelf'
 
-class BookList extends Component {
-  render() {
-    const shelves = [
-      {
-        status: 'currentlyReading',
-        name: 'Currently Reading'
-      }, {
-        status: 'wantToRead',
-        name: 'Want to Read'
-      }, {
-        status: 'read',
-        name: 'Read'
-      }
-    ]
+// Stateless Functional Component
+const BookList = (props) => {
+  const {books} = props
+  const shelves = [
+    {
+      type: 'currentlyReading',
+      title: 'Currently Reading'
+    }, {
+      type: 'wantToRead',
+      title: 'Want to Read'
+    }, {
+      type: 'read',
+      title: 'Read'
+    }
+  ]
 
-    return (
-      <div className="list-books-content"> {
-        shelves.map((shelf) => {
-          return (
-            <div key={shelf.status}>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">{shelf.name}
-
-              </h2>
-              <BookShelf />
+  return (<div className="list-books-content">
+    {
+      shelves.map((shelf) => {
+        // finding book by category in the originally fetched array
+        const filteredBooks = books.filter(book => book.shelf === shelf.type)
+        console.log(filteredBooks)
+        
+        return (<div key={shelf.type}>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelf.title}
+            </h2>
+            <div className="bookshelf-books">
+              <BookShelf filteredBooks={filteredBooks}/>
             </div>
-          </div>)
-        })
-      }
-    </div>)
-  }
+          </div>
+        </div>)
+      })
+    }
+  </div>)
 }
 
 export default BookList
