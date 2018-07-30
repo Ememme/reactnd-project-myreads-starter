3f8bfae8ef8b2dce06abcead68eaee9c688d8e31
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-07-30 12:51:53 +0200

    Fix: improved iterating and mapping over books on shelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -3,13 +3,18 @@ import Book from './Book'
 
 class BookShelf extends Component {
   render() {
-
+    console.log(this.props)
+    console.log(this.props.addBook)
     return(
+
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <Book />
-            <Book />
-            <Book />
+            {/*iterating over books on shelf to get data about single book  */}
+            {this.props.filteredBooks.map((book) => (
+            <Book filteredBook={book}
+                  key={book.id}
+            />
+        ))}
           </ol>
         </div>
     )
