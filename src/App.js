08dcec0aa24e339cea5f45af4816cc8dc9079949
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-07-30 16:33:49 +0200

    Feat: add routing for main page and searchBooks component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './BookList';
 import Library from './Library';
+import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
   state = {
@@ -10,22 +12,24 @@ class BooksApp extends React.Component {
   }
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-        this.setState({books: books})
+      this.setState({books: books})
     })
   }
   render() {
     console.log(this.state.books)
     return (
       <div className="app">
+        <Route path="/search" component={SearchBooks}/>
+        <Route exact path="/" render={() => (
           <div className="list-books">
             <Library  />
             <BookList books={this.state.books} />
 
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link to="search">Add a book</Link>
             </div>
           </div>
-        {/* )} */}
+        )}/>
       </div>
     )
   }
