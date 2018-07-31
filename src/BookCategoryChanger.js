50de5bf9146fa833c7719bcbea0725f94b5b2730
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-07-31 18:56:31 +0200

    Fix: update value to reflect shelf position

--- src/BookCategoryChanger.js
+++ src/BookCategoryChanger.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
 
+
 class BookCategoryChanger extends Component {
   render() {
     return (<div className="book-shelf-changer">
-      <select>
+      <select onChange={(event) => {this.props.changeShelf(this.props.bookInfo.key, event.target.value)} } value={this.props.bookInfo.shelf}>
         <option value="none" disabled="disabled">Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
