fca442fc268492c09fc233bf4772f9234537836c
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-03 14:18:18 +0200

    Fix: add default none shelf value in the Search

--- src/BookCategoryChanger.js
+++ src/BookCategoryChanger.js
@@ -4,8 +4,10 @@ import React, {Component} from 'react'
 class BookCategoryChanger extends Component {
   render() {
     console.log(this.props.bookInfo)
+    // Set shelf value to none if it's not defined
+    const shelfValue = this.props.bookInfo.shelf? this.props.bookInfo.shelf : 'none'
     return (<div className="book-shelf-changer">
-      <select onChange={(event) => {this.props.changeShelf(this.props.bookInfo, event.target.value)} } value={this.props.bookInfo.shelf}>
+      <select onChange={(event) => {this.props.changeShelf(this.props.bookInfo, event.target.value)} } value={shelfValue}>
         <option value="none" disabled="disabled">Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
