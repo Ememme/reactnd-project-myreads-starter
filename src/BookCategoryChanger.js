b2b380126ec2477c9315cdee291893c6ea03583d
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-03 14:43:20 +0200

    Fix: corrected default shelf value - none was defined twice in source code

--- src/BookCategoryChanger.js
+++ src/BookCategoryChanger.js
@@ -5,10 +5,10 @@ class BookCategoryChanger extends Component {
   render() {
     console.log(this.props.bookInfo)
     // Set shelf value to none if it's not defined
-    const shelfValue = this.props.bookInfo.shelf? this.props.bookInfo.shelf : 'none'
+    const shelfValue = this.props.bookInfo.shelf? this.props.bookInfo.shelf : 'move'
     return (<div className="book-shelf-changer">
       <select onChange={(event) => {this.props.changeShelf(this.props.bookInfo, event.target.value)} } value={shelfValue}>
-        <option value="none" disabled="disabled">Move to...</option>
+        <option value="move" disabled="disabled">Move to...</option>
         <option value="currentlyReading">Currently Reading</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
